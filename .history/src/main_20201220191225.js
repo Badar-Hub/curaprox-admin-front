674ddf24ef26d6e7c21a951641faf48c3c186379
Badar <bapk65@gmail.com>
2021-01-03 22:25:19 +0500

    wip

--- .history/src/main_20201220191225.js
+++ .history/src/main_20201220191225.js
@@ -1,6 +1,8 @@
 import Vue from "vue";
 import App from "./App.vue";
+import Antd from "ant-design-vue";
 import CKEditor from "@ckeditor/ckeditor5-vue";
+import "ant-design-vue/dist/antd.css";
 import { MdButton, MdContent, MdTabs } from "vue-material/dist/components";
 import "vue-material/dist/vue-material.min.css";
 import "vue-material/dist/theme/default.css";
@@ -10,6 +12,9 @@ Vue.config.productionTip = false;
 Vue.use(MdButton);
 Vue.use(MdContent);
 Vue.use(MdTabs);
+
+Vue.use(Antd);
+
 Vue.use(CKEditor);
 
 new Vue({
